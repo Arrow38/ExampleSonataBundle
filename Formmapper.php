038e2df40d553129a17dd2e6e0d68d646fafc5bb
sico974 <etienne.vaytilingom@gmail.com>
2018-07-10 11:01:35 +0400

    Update and rename Formmapper.md to Formmapper.php

--- Formmapper.php
+++ Formmapper.php
@@ -1,9 +1,9 @@
 $formMapper
 
-* Int with min and max
+// * Int with min and max
 ->add('numportable2','integer', ['attr' => array('min' => 692000000, 'max' => 693999999),'label' => 'Numéro de portable 2','required'=>false])
 
-* Get original data before the object updated 
+// * Get original data before the object updated 
 
   public function preUpdate($object){
         $em = $this->getModelManager()->getEntityManager($this->getClass());
