0294c26bd6d07823f6b0ee7e86bbc767f6091bf3
sico974 <etienne.vaytilingom@gmail.com>
2018-07-12 09:16:16 +0400

    Update Formmapper.php

--- Formmapper.php
+++ Formmapper.php
@@ -10,3 +10,24 @@ $formMapper
         $original = (object)  $em->getUnitOfWork()->getOriginalEntityData($object);
         $original-> mission->getName() ;
         }
+
+// * OnetoMany side to update or persist 
+// source : https://github.com/sonata-project/SonataAdminBundle/issues/165#issuecomment-1772219
+public function prePersist($object)
+{
+    $object->setPictures($object->getPictures());
+}
+
+public function preUpdate($object)
+{
+    $object->setPictures($object->getPictures());
+}
+
+    public function setPictures($pictures)
+    {
+        $this->pictures = $pictures;
+
+        foreach ($pictures as $picture) {
+            $picture->setProduct($this);
+        }
+    }
